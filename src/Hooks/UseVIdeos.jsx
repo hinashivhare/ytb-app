1b6dc79e72d4e5766a95bf92e9323fc0e4b8fb49
Hina Shivhare <hinashivhare@Hinas-MacBook-Pro.local>
2020-12-08 17:13:50 -0500

    ready for deploy

---
+++ src/Hooks/UseVIdeos.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import youtube from "../api/youtube";
+
+const UseVideos = (defaultSearchTerm) => {
+    const [videos,setVideos] = useState([]);
+
+    useEffect(() => {
+        search(defaultSearchTerm);
+    },[defaultSearchTerm]) ;
+
+    const search = async (term) => {
+        const response = await youtube.get('/search',{
+            params: {
+                q: term
+            }
+        });
+        setVideos(response.data.items);
+    };
+    return [videos,search];
+};
+
+export default UseVideos;
